d27416b350859ab680bbf655aeeee277f73aae4d
hangry <hangrybarboons@gmail.com>
2025-02-09 17:53:42 -0500

    Complete overhaul

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function openSidebar() {
-    document.getElementById("sidebar").classList.add("open");
-}
+const sidebar = document.getElementById("sidebar");
+const filterToggle = document.getElementById("filter-toggle");
+const closeSidebar = document.getElementById("close-sidebar");
 
-function closeSidebar() {
-    document.getElementById("sidebar").classList.remove("open");
-}
+filterToggle.addEventListener("click", () => {
+    sidebar.classList.add("open");
+});
+
+closeSidebar.addEventListener("click", () => {
+    sidebar.classList.remove("open");
+});

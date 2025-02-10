5522f07c67ba3b89c965f0bb2c1262eadde027ef
hangry <hangrybarboons@gmail.com>
2025-02-10 00:53:53 -0500

    Updated Project Files

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const sidebar = document.getElementById("sidebar");
-const filterToggle = document.getElementById("filter-toggle");
-const closeSidebar = document.getElementById("close-sidebar");
-
-// Open sidebar on mobile
-filterToggle.addEventListener("click", () => {
-    sidebar.classList.add("open");
-});
-
-// Close sidebar on mobile
-closeSidebar.addEventListener("click", () => {
-    sidebar.classList.remove("open");
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebar = document.getElementById("sidebar");
+    const filterToggle = document.getElementById("filter-toggle");
+    const closeSidebar = document.getElementById("close-sidebar");
+  
+    // Open sidebar on mobile
+    filterToggle.addEventListener("click", () => {
+      sidebar.classList.add("open");
+    });
+  
+    // Close sidebar on mobile
+    closeSidebar.addEventListener("click", () => {
+      sidebar.classList.remove("open");
+    });
+  });
+  

aebc8e74c6515e39f528e968755c609c0a3e4170
hangry <hangrybarboons@gmail.com>
2025-02-09 23:43:35 -0500

    Updated colors, added placeholder NFTs, and fixed menu

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const sidebar = document.getElementById("sidebar");
 const filterToggle = document.getElementById("filter-toggle");
 const closeSidebar = document.getElementById("close-sidebar");
 
+// Open sidebar on mobile
 filterToggle.addEventListener("click", () => {
     sidebar.classList.add("open");
 });
 
+// Close sidebar on mobile
 closeSidebar.addEventListener("click", () => {
     sidebar.classList.remove("open");
 });
